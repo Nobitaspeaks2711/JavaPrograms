089d9b5bee892b4657b7171961173c17d171af67
Nobitaspeaks2711 <vaibhavbauti99@gmail.com>
2024-09-05 13:22:59 +0530

    added java Star pattern programs

--- PatternPrograms/src/StarPatternP1.java
+++ PatternPrograms/src/StarPatternP1.java
@@ -1,2 +1,12 @@
 public class StarPatternP1 {
+    public static void main (String[]args){
+
+        //ex1
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
